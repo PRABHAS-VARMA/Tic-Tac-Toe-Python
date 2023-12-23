00b38dd9599d0413a87c2ce0ec0129bc8f4b2436
PRABHAS-VARMA <141707979+PRABHAS-VARMA@users.noreply.github.com>
2023-12-23 17:53:56 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tic-Tac-Toe-Python
+
+[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/PRABHAS-VARMA/Tic-Tac-Toe-Python)
---
+++ main.py
@@ -0,0 +1,57 @@
+def grid(x):
+    for i in x:
+        print(" | ".join(i))
+        print("-" * 9)
+
+def win(grid):
+    for i in grid:
+        if i[0] == i[1] == i[2] and i[0] != ' ':
+            return True
+
+    for j in range(3):
+        if grid[0][j] == grid[1][j] == grid[2][j] and grid[0][j] != ' ':
+            return True
+
+    if grid[0][0] == grid[1][1] == grid[2][2] and grid[0][0] != ' ':
+        return True
+    if grid[0][2] == grid[1][1] == grid[2][0] and grid[0][2] != ' ':
+        return True
+
+    return False
+
+def fillingGrid(grid):
+    for i in grid:
+        if ' ' in i:
+            return False
+    return True
+
+def main():
+    a = [[' ' for _ in range(3)] for _ in range(3)]
+    presentPlayer = 'X'
+
+    while True:
+        grid(a)
+
+        row = int(input(f"Player {presentPlayer}, enter row (0, 1, or 2): "))
+        col = int(input(f"Player {presentPlayer}, enter column (0, 1, or 2): "))
+
+        if a[row][col] == ' ':
+            a[row][col] = presentPlayer
+        else:
+            print("Cell already taken. Try again.")
+            continue
+
+        if win(a):
+            grid(a)
+            print(f"Player {presentPlayer} wins!")
+            break
+
+        if fillingGrid(a):
+            grid(a)
+            print("It's a tie!")
+            break
+
+        presentPlayer = 'O' if presentPlayer == 'X' else 'X'
+
+if __name__ == "__main__":
+    main()
